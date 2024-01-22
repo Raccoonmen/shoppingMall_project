10dfcfa36f69424e13ce16bea8c8a543be3b3ecf
Nayeob <108695511+Raccoonmen@users.noreply.github.com>
2024-01-22 22:42:58 +0900

    feat/test : 로그인
    JWT 토큰 발급, 인증파트 추가
    간단한 Jwt 토큰 발급, 인증 테스트 추가

---
+++ backend/src/main/java/com/example/shopping/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.example.shopping.config;
+
+import com.example.shopping.security.jwt.exception.CustomAuthenticationEntryPoint;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsUtils;
+
+
+
+// Spring Security에 대한 설정
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final AuthenticationManagerConfig authenticationManagerConfig;
+
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin().disable()
+                .csrf().disable()
+                .cors()
+                .and()
+                .apply(authenticationManagerConfig)
+                .and()
+                .httpBasic().disable()
+                .authorizeRequests()
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                .mvcMatchers("/signup", "/login", "/users/refresh").permitAll()
+                .mvcMatchers(HttpMethod.GET, "/**").hasAnyRole("USER", "MANAGER", "ADMIN")
+                .mvcMatchers(HttpMethod.POST, "/**").hasAnyRole("USER", "MANAGER", "ADMIN")
+                .anyRequest().hasAnyRole()
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(customAuthenticationEntryPoint)
+                .and()
+                .build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
